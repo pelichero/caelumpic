b519a592a66556c84f8ee699b66aefabf604d7a1
fepelitchero <pelichero_13@hotmail.com>
2018-01-16 20:04:56 -0200

    Incluindo endereco da api local

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -11,7 +11,8 @@ export class AppComponent {
     fotos: Object[] = []
 
     constructor(http: Http) {
-        http.get('http://demo0313340.mockable.io/images')
+        // http.get('http://demo0313340.mockable.io/images')
+        http.get('http://localhost:3000/v1/fotos')
             .map(res => res.json())
             .subscribe(
                 fotos => this.fotos = fotos,
