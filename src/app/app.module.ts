858b027a5a0bfea20a1c4b4401eed830d53c8f29
fepelitchero <pelichero_13@hotmail.com>
2018-01-17 19:44:06 -0200

    Incluindo rotas

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,23 +1,27 @@
 //	caelumpic/src/app/app.module.ts
 import 'rxjs/add/operator/map';
-import	{	NgModule	}	from	'@angular/core';
-import	{	BrowserModule	}	from	'@angular/platform-browser';
-import	{	HttpModule	}	from	'@angular/http';
-import	{	AppComponent	}	from	'./app.component';
-import	{	FotoModule	}	from	'./foto/foto.module';
-import	{	PainelModule	}	from	'./painel/painel.module';
-import	{	ListagemComponent	}	from	'./listagem/listagem.component';
-import	{	routing	}	from	'./app.routes';
+//	caelumpic/src/app/app.module.ts
+import 'rxjs/add/operator/map';
+import {NgModule} from '@angular/core';
+import {BrowserModule} from '@angular/platform-browser';
+import {HttpModule} from '@angular/http';
+import {AppComponent} from './app.component';
+import {FotoModule} from './foto/foto.module';
+import {PainelModule} from './painel/painel.module';
+import {ListagemComponent} from './listagem/listagem.component';
+import {routing} from './app.routes';
+
 @NgModule({
-    imports:						[
+    imports: [
         BrowserModule,
         FotoModule,
         HttpModule,
         PainelModule,
-        routing	],
-    declarations:	[
+        routing],
+    declarations: [
         AppComponent,
-        ListagemComponent	],
-    bootstrap:				[	AppComponent	]
+        ListagemComponent],
+    bootstrap: [AppComponent]
 })
-export class	AppModule	{	}
+export class AppModule {
+}
