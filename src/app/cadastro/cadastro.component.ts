11bc12138df819162e4b872eba75b085a58ad59a
fepelitchero <pelichero_13@hotmail.com>
2018-01-19 20:37:25 -0200

    adicionando o pipe

--- src/app/cadastro/cadastro.component.ts
+++ src/app/cadastro/cadastro.component.ts
@@ -3,6 +3,7 @@ import {Component} from '@angular/core'
 import {FotoComponent} from '../foto/foto.component'
 import {FotoService} from "../servicos/foto.service"
 import {ActivatedRoute, Router} from "@angular/router";
+import {FormBuilder, FormGroup, Validators} from "@angular/forms";
 
 @Component({
     selector: 'cadastro',
@@ -12,13 +13,22 @@ export class CadastroComponent {
     foto: FotoComponent = new FotoComponent()
     service: FotoService
     mensagem: String
+    fotoForm: FormGroup
 
-    constructor(service: FotoService, private activatedRoute: ActivatedRoute, private router: Router) {
+    constructor(service: FotoService, private activatedRoute: ActivatedRoute, private router: Router, private formBuilder: FormBuilder) {
         this.service = service
         this.activatedRoute.params.subscribe(p => {
             if (p.id) {
                 this.carregaFotoId(p.id)
             }
+        })
+
+        this.fotoForm = this.formBuilder.group({
+            titulo:['', Validators.compose(
+                [Validators.required,	Validators.minLength(4)]
+            )],
+            url:['', Validators.required],
+            descricao:['', Validators.required]
         })
     }
 
--- src/app/foto/foto.module.ts
+++ src/app/foto/foto.module.ts
@@ -2,11 +2,12 @@
 import {NgModule} from '@angular/core'
 import {FotoComponent} from './foto.component'
 import {FotoService} from '../servicos/foto.service';
+import {FiltroPorTitulo} from './foto.pipes';
 
 @NgModule({
-    declarations: [FotoComponent],
-    exports: [FotoComponent],
-    providers:	[	FotoService	]
+    declarations: [FotoComponent, FiltroPorTitulo],
+    exports: [FotoComponent, FiltroPorTitulo],
+    providers: [FotoService]
 })
 export class FotoModule {
 }
